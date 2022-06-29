115708ffe6e4bad48f91018223b6fcb3f8e0dd00
vyom <sharm843@umn.edu>
2022-06-29 15:43:50 +0530

    Commit 2 - More Testing examples

--- Jest-Testing/functions.js
+++ Jest-Testing/functions.js
@@ -1,5 +1,22 @@
+const axios = require('axios');
+
+
+const { default: axios } = require("axios");
+
 const functions = {
-    add: (num1, num2) => num1 + num2
+    add: (num1, num2) => num1 + num2,
+    isNull: () => null,
+    checkValue: (x) => x,
+    createUser: () => {
+        const user = { firstName: 'Vyom' } 
+        user['lastName'] = 'Sharma';
+        return user
+    },
+    fetchUser: () => {
+        axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.data)
+        .catch(err => 'There is an error')
+    }
 }
 
 module.exports = functions;
